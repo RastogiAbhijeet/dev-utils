015f079c4fceacdae47fe6115219aa98727f8ead
RastogiAbhijeet <abhijeetrastogi1997@gmail.com>
2021-08-21 23:17:53 +0530

    Add delete branch util

---
+++ git-cli/README.md
@@ -0,0 +1,15 @@
+# Git Cli Utils
+
+## Delete branch utils
+
+### **Examples**
+---
+
+#### Deleting feature branches with pattern feature/*
+`bash utils.sh delete-feature-branches`
+
+#### Deleting fix branches with pattern fix/*
+`bash utils.sh delete-fix-branches`
+
+#### Deleting branch with patterns
+`bash utils.sh delete-branch-pattern random-branch-pattern`
--- git-cli/utils.sh
+++ git-cli/utils.sh
@@ -1,6 +1,45 @@
-delete_feature_branches () {
-  args2=$2
-  git branch -d $(git branch --list $(pattern))
+#!/bin/bash
+
+# Arguments
+level_one_arg=$1
+
+delete_branch_pattern() {
+  local branch_pattern=$1
+
+  if [ "$branch_pattern" ]; then
+    branches=$(git branch --list "$branch_pattern")
+    if [ "$branches" ]; then
+      git branch -d $branches
+    else
+      echo No branch matching $branch_pattern
+    fi
+
+  else
+    echo "Missing branch pattern"
+
+  fi
 }
 
-echo $1, $2
+delete_feature_branches() {
+  if [ $level_one_arg == 'delete-feature-branches' ]; then
+    delete_branch_pattern feature/*
+  fi
+}
+
+delete_fix_branches() {
+  if [ $level_one_arg == 'delete-fix-branches' ]; then
+    delete_branch_pattern fix/*
+  fi
+}
+
+main() {
+  if [ $level_one_arg == 'delete-feature-branches' ]; then
+    delete_feature_branches
+  elif [ $level_one_arg == 'delete-fix-branches' ]; then
+    delete_fix_branches
+  elif [ $level_one_arg == 'delete-branch-pattern' ]; then
+    delete_branch_pattern $2
+  fi
+}
+
+main $1 $2
